c50674659c26628cce3dd4effd0205803ca46772
mscj1004 <mscj1004@kaist.ac.kr>
2022-07-07 18:37:20 +0900

    ADD : findUser

--- src/controller/UserListController.ts
+++ src/controller/UserListController.ts
@@ -15,4 +15,28 @@ export class UserListController{
         console.log(result);
         res.send(result);
     }
+
+    static findUser = async (req, res) => {
+        console.log(req.body)
+        const id = req.body.id;
+        const password = req.body.password;
+        const result = await getConnection().getRepository(UserList).findOne({where:{id}});
+        const result2 = await getConnection().getRepository(UserList).findOne({where:{password}});
+
+        if (result2 ==null){
+            return res.status(200).send({ loginSuccess : false, message: "비밀번호가 없습니다."});
+        }
+
+        else if(result == null){
+            return res.status(200).send({ loginSuccess : false, message: "아이디가 없습니다."});
+        }
+
+        else if(id ==result.id && password == result2.password){
+            return res.status(200).send({ loginSuccess: true, message: "로그인 성공"});
+
+        }
+        console.log(result);
+        res.send(result);
+
+    }
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,4 +3,5 @@ import {UserListController} from "../controller/UserListController";
 
 const router = Router();
 router.post('/user', UserListController.addUser);
+router.post('/user1', UserListController.findUser);
 export default router;
